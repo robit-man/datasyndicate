223a9f17aea9fa3556edeb94954769c2d565d659
pointblank <pointblank.connect@gmail.com>
2021-10-12 17:48:28 +0000

    ETH

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -192,9 +192,7 @@ import {
           <div transition:slide class="description">
             <p class="mediumtext" style="font-weight:100;margin:unset!important;">PURCHASING</p>
             <p class="mediumtext" style="font-weight:900;margin:unset!important;"><b>INFORMATION</b></p>
-            <p> <b>Purchasing a MANDEL VIEW will cost ΦΞ (1.618033988749894848 ETH)each</b>
-              <br><br>
-              Owning a VIEW will grant you access to monthly art raffles air dropping from MANDEL Curated Collections, and minters who hold until 2022 are eligible to claim a high quality physically framed art print out mailed for free.
+            <p> <b>Purchasing a MANDEL VIEW will cost ΦΞ (1.618033988749894848 ETH each.</b>
             </p>
           </div>
         </div>
